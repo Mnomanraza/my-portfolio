57d9bfb87df0be09dec5bbd78ebaa08e294ff8be
Mnomanraza <161114078+Mnomanraza@users.noreply.github.com>
2024-03-20 02:17:49 +0500

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,13 @@
+var cricket=Math.random()*7;
+var roundoffnum= Math.floor(cricket);
+var looser = 0; 
+if (looser==roundoffnum){
+
+    alert("you are out");
+
+}
+
+else {
+alert(roundoffnum) ;
+
+}
